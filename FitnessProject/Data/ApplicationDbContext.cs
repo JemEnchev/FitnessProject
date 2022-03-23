2e223b23076ae391b62fb4828e44df947ffb11ce
JemEnchev <dsenchev@abv.bg>
2022-03-24 00:51:28 +0200

    Add project files.

---
+++ FitnessProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessProject.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
