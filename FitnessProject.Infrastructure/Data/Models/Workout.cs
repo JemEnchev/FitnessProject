9eafd2edb43f153a6489fcc3ab9649c1d46c0556
JemEnchev <dsenchev@abv.bg>
2022-04-05 21:46:36 +0300

    Added Workout model

---
+++ FitnessProject.Infrastructure/Data/Models/Workout.cs
@@ -0,0 +1,29 @@
+﻿namespace FitnessProject.Infrastructure.Data.Models
+{
+    using FitnessProject.Infrastructure.Data.Identity;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Workout
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+
+        public ICollection<WorkoutExercise> WorkoutExercises { get; set; }
+    }
+}
