d6e42a0a16c3c744c11c49f4d515491c791b25ce
JemEnchev <dsenchev@abv.bg>
2022-04-05 21:49:40 +0300

    Modified ApplicationUser and DbContext

--- FitnessProject.Infrastructure/Data/ApplicationDbContext.cs
+++ FitnessProject.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace FitnessProject.Infrastructure.Data
 {
+    using FitnessProject.Infrastructure.Data.Models;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
@@ -9,5 +10,63 @@
             : base(options)
         {
         }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SupplementBrand>()
+               .HasIndex(b => b.Name)
+               .IsUnique();
+            
+            modelBuilder.Entity<SupplementFlavour>()
+               .HasIndex(f => f.Name)
+               .IsUnique(); 
+            
+            modelBuilder.Entity<Supplement>()
+               .HasIndex(s => s.Name)
+               .IsUnique();
+
+            modelBuilder.Entity<Exercise>()
+               .HasIndex(e => e.Name)
+               .IsUnique();
+
+            modelBuilder.Entity<Food>()
+               .HasIndex(f => f.Name)
+               .IsUnique();
+
+
+
+            modelBuilder.Entity<WorkoutExercise>()
+                .HasKey(we => new { we.ExerciseId, we.WorkoutId });
+
+            modelBuilder.Entity<DietFood>()
+                .HasKey(df => new { df.DietPlanId, df.FoodId });
+
+            modelBuilder.Entity<DietSupplement>()
+                .HasKey(ds => new { ds.DietPlanId, ds.SupplementId });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Diet> DietPlans { get; set; }
+        
+        public DbSet<DietFood> DietPlanFoods { get; set; }
+        
+        public DbSet<DietSupplement> DietPlanSupplements { get; set; }
+        
+        public DbSet<Exercise> Exercises { get; set; }
+        
+        public DbSet<Food> Foods { get; set; }
+        
+        public DbSet<Supplement> Supplements { get; set; }
+        
+        public DbSet<SupplementBrand> SupplementBrands { get; set; }
+        
+        public DbSet<SupplementFlavour> SupplementFlavours { get; set; }
+        
+        public DbSet<Workout> Workouts { get; set; }
+        
+        public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
     }
 }
---
+++ FitnessProject.Infrastructure/Data/Identity/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿namespace FitnessProject.Infrastructure.Data.Identity
+{
+    using FitnessProject.Infrastructure.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+
+        public ICollection<Workout> Workouts { get; set; } = new List<Workout>();
+
+        public ICollection<Diet> Diets { get; set; } = new List<Diet>();
+    }
+}
