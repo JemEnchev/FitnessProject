16bc06edc8a47f07471920e81f9931386ea64358
JemEnchev <dsenchev@abv.bg>
2022-04-05 13:17:36 +0300

    Added Town model

---
+++ FitnessProject.Infrastructure/Data/Town.cs
@@ -0,0 +1,16 @@
+﻿namespace FitnessProject.Infrastructure.Data
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Town
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        public ICollection<Address> addresses { get; set; } = new List<Address>();
+    }
+}
