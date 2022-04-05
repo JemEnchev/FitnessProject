5bee5fe8b8ac04559eaf938bea944f3fb3d8de09
JemEnchev <dsenchev@abv.bg>
2022-04-05 21:48:01 +0300

    Added some enums

---
+++ FitnessProject.Infrastructure/Data/Models/Enums/ExerciseDifficulty.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessProject.Infrastructure.Data.Models.Enums
+{
+    public enum ExerciseDifficulty
+    {
+        VeryEasy = 0,
+        Easy = 2,
+        Normal = 3,
+        Hard = 4,
+        VeryHard = 5,
+    }
+}
---
+++ FitnessProject.Infrastructure/Data/Models/Enums/SupplementType.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessProject.Infrastructure.Data.Models.Enums
+{
+    public enum SupplementType
+    {
+        Pills = 0,
+        Poweder = 1,
+        Tablets = 2,
+        Capsules = 3,
+        Gummies = 4,
+        Drink = 5,
+    }
+}
