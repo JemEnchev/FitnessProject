11f383ebb8d72c98c4b70fc1097b4ed94f0943bd
JemEnchev <dsenchev@abv.bg>
2022-04-05 15:44:06 +0300

    Added supplement type, flavour and brand

---
+++ FitnessProject.Infrastructure/Data/Models/SupplementFlavour.cs
@@ -0,0 +1,17 @@
+﻿namespace FitnessProject.Infrastructure.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SupplementFlavour
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [StringLength(300)]
+        public string? Description { get; set; }
+    }
+}
---
+++ FitnessProject.Infrastructure/Data/Models/SupplementType.cs
@@ -0,0 +1,17 @@
+﻿namespace FitnessProject.Infrastructure.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SupplementType
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(100)]
+        public string? Type { get; set; }
+
+        [StringLength(300)]
+        public string? Description { get; set; }
+    }
+}
