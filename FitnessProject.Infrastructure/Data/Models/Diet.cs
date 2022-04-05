d7336c33e7f3eb845b5114929dd0e40d3d301f83
JemEnchev <dsenchev@abv.bg>
2022-04-05 21:46:14 +0300

    Added Diet model

---
+++ FitnessProject.Infrastructure/Data/Models/Diet.cs
@@ -0,0 +1,32 @@
+﻿namespace FitnessProject.Infrastructure.Data.Models
+{
+    using FitnessProject.Infrastructure.Data.Identity;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Diet
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+
+        public ICollection<DietFood> DietFoods { get; set; }
+        
+        public ICollection<DietSupplement> DietSupplements { get; set; }
+
+    }
+}
