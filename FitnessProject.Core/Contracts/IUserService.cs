0e6ea1c1b8fd165a0a2e746c80ffc647ea499f41
JemEnchev <dsenchev@abv.bg>
2022-04-12 16:26:01 +0300

    Added UserService

---
+++ FitnessProject.Core/Contracts/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessProject.Core.Contracts
+{
+    using FitnessProject.Core.Models;
+
+    public interface IUserService
+    {
+        Task<IEnumerable<UserList_VM>> GetUsersAsync();
+    }
+}
---
+++ FitnessProject.Core/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿namespace FitnessProject.Core.Services
+{
+    using FitnessProject.Core.Contracts;
+    using FitnessProject.Core.Models;
+    using FitnessProject.Infrastructure.Data.Identity;
+    using FitnessProject.Infrastructure.Data.Repositories;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class UserService : IUserService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        public UserService(IApplicationDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+
+        public async Task<IEnumerable<UserList_VM>> GetUsersAsync()
+        {
+            return await repo.All<ApplicationUser>()
+                .Select(u => new UserList_VM()
+                {
+                    Id = u.Id,
+                    Name = $"{u.FirstName} {u.LastName}",
+                    Email = u.Email
+                })
+                .ToListAsync();
+        }
+    }
+}
