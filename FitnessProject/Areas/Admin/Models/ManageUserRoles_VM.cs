a53905acb5e47ccf2928873ede19fd91ebb7b761
JemEnchev <dsenchev@abv.bg>
2022-04-12 16:43:50 +0300

    Added UserRoles controller, views (Index, Manage) and their needed ViewModels (UserRoles, ManageUserRoles)

---
+++ FitnessProject/Areas/Admin/Models/ManageUserRoles_VM.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessProject.Areas.Admin.Models
+{
+    public class ManageUserRoles_VM
+    {
+        public string RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
