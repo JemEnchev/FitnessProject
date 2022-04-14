0b80fe99a807f8c39104812534c80dcee6aa29a3
JemEnchev <dsenchev@abv.bg>
2022-04-14 12:01:45 +0300

    Moved model from admin area to core project

---
+++ FitnessProject.Core/Models/RoleList_VM.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessProject.Core.Models
+{
+    public class RoleList_VM
+    {
+        public string RoleId { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
---
+++ FitnessProject.Core/Models/UserRoles_VM.cs
@@ -0,0 +1,17 @@
+﻿namespace FitnessProject.Core.Models
+{
+    public class UserRoles_VM
+    {
+        public string UserId { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string UserName { get; set; }
+        
+        public string Email { get; set; }
+        
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
