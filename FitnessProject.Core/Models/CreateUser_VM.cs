5e42d58c5d566e34a7693218accca0040b1a5aff
JemEnchev <dsenchev@abv.bg>
2022-04-24 16:51:06 +0300

    Added CreateUser_VM

---
+++ FitnessProject.Core/Models/CreateUser_VM.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessProject.Core.Models
+{
+    public class CreateUser_VM
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { set; get; }
+    }
+}
