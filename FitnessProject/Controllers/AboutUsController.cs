55bb86ae8a5634838ef2ba122fcfdf8e60ba7439
JemEnchev <dsenchev@abv.bg>
2022-04-25 21:29:29 +0300

    Created AboutController

---
+++ FitnessProject/Controllers/AboutUsController.cs
@@ -0,0 +1,17 @@
+﻿namespace FitnessProject.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AboutUsController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
