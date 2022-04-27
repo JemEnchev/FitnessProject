8222f58445e217a03c45cca1261e411c2c3a3fa0
JemEnchev <dsenchev@abv.bg>
2022-04-28 02:08:32 +0300

    Added ExerciseService

---
+++ FitnessProject.Core/Contracts/IExerciseService.cs
@@ -0,0 +1,20 @@
+﻿namespace FitnessProject.Core.Contracts
+{
+    using FitnessProject.Core.Models;
+    using FitnessProject.Infrastructure.Data.Models;
+
+    public interface IExerciseService
+    {
+        Task<IEnumerable<Exercise>> GetAllExercisesAsync();
+
+        Task<IEnumerable<Exercise>> GetAllFavouritesAsync(string userEmail);
+
+        Task AddExerciseAsync(AddExercise_VM model);
+
+        Task RemoveExerciseAsync(string exerciseName);
+
+        Task AddToFavouritesAsync(string exerciseName, string userEmail);
+
+        Task RemoveFromFavouritesAsync(string exerciseName, string userEmail);
+    }
+}
---
+++ FitnessProject.Core/Services/ExerciseService.cs
@@ -0,0 +1,180 @@
+﻿namespace FitnessProject.Core.Services
+{
+    using FitnessProject.Core.Contracts;
+    using FitnessProject.Core.Models;
+    using FitnessProject.Infrastructure.Data.Identity;
+    using FitnessProject.Infrastructure.Data.Models;
+    using FitnessProject.Infrastructure.Data.Repositories;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class ExerciseService : IExerciseService
+    {
+        private readonly IApplicationDbRepository repo;
+
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        private readonly IUserManagerService userManagerService;
+
+        public ExerciseService(
+            IApplicationDbRepository _repo,
+            IWebHostEnvironment _webHostEnvironment,
+            IUserManagerService _userManagerService)
+        {
+            repo = _repo;
+            webHostEnvironment = _webHostEnvironment;
+            userManagerService = _userManagerService;
+        }
+
+        public async Task AddExerciseAsync(AddExercise_VM model)
+        {
+            string stringFileName = UploadFile(model);
+
+            var exercise = new Exercise()
+            {
+                Name = model.Name,
+                Category = model.Category,
+                Description = model.Description,
+                IsItBodyweight = model.IsItBodyweight,
+                Requirements = model.Requirements,
+                Difficulty = model.Difficulty,
+                Image = stringFileName
+            };
+
+            if (repo.All<Exercise>().FirstOrDefault(f => f.Name == exercise.Name) != null)
+            {
+                return;
+            }
+
+            await repo.AddAsync(exercise);
+            await repo.SaveChangesAsync();
+        }
+
+        private string UploadFile(AddExercise_VM model)
+        {
+            string fileName = null;
+            if (model.Image != null)
+            {
+                string uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "img");
+                fileName = Guid.NewGuid().ToString() + "-" + model.Image.FileName;
+                string filePath = Path.Combine(uploadDir, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Image.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
+
+        public async Task<IEnumerable<Exercise>> GetAllExercisesAsync()
+        {
+            return await repo.All<Exercise>()
+                 //.Select(e => new AddExercise_VM()
+                 //{
+                 //    Name = e.Name,
+                 //    Category = e.Category,
+                 //    Description = e.Description,
+                 //    IsItBodyweight = e.IsItBodyweight,
+                 //    Requirements = e.Requirements,
+                 //    Difficulty = e.Difficulty,
+                 //    Image = e.Image
+                 //})
+                 .ToListAsync();
+        }
+
+        public async Task RemoveExerciseAsync(string exerciseName)
+        {
+            var exercise = await GetExerciseByNameAsync(exerciseName);
+
+            var imagePath = Path.Combine(webHostEnvironment.WebRootPath, "img", exercise.Image);
+
+            try
+            {
+                repo.Delete(exercise);
+                await repo.SaveChangesAsync();
+
+                File.Delete(imagePath);
+            }
+            catch (Exception)
+            { }
+        }
+
+        private async Task<Exercise> GetExerciseByNameAsync(string exerciseName)
+        {
+            return await repo.All<Exercise>()
+                .FirstOrDefaultAsync(e => e.Name == exerciseName);
+        }
+
+        public async Task AddToFavouritesAsync(string exerciseName, string userEmail)
+        {
+            var exercise = await GetExerciseByNameAsync(exerciseName);
+
+            var user = await userManagerService.GetUserByEmailAsync(userEmail);
+
+            if (exercise != null && user != null)
+            {
+                var userExercise = new UserExercise()
+                {
+                    User = user,
+                    UserId = user.Id,
+                    Exercise = exercise,
+                    ExerciseId = exercise.Id
+                };
+
+                if (!repo.All<UserExercise>().Contains(userExercise))
+                {
+                    try
+                    {
+                        await repo.AddAsync(userExercise);
+                        await repo.SaveChangesAsync();
+                    }
+                    catch (Exception)
+                    { }
+                }
+            }
+        }
+
+        public async Task<IEnumerable<Exercise>> GetAllFavouritesAsync(string userEmail)
+        {
+            var user = await userManagerService.GetUserByEmailAsync(userEmail);
+
+            return await repo.All<UserExercise>()
+                .Where(u => u.User.Email == userEmail)
+                .Select(e => new Exercise()
+                {
+                    Name = e.Exercise.Name,
+                    Category = e.Exercise.Category,
+                    IsItBodyweight = e.Exercise.IsItBodyweight,
+                    Requirements = e.Exercise.Requirements,
+                    Difficulty = e.Exercise.Difficulty,
+                    Description = e.Exercise.Description,
+                    Image = e.Exercise.Image,
+                })
+                .ToListAsync();
+        }
+
+        public async Task RemoveFromFavouritesAsync(string exerciseName, string userEmail)
+        {
+            var exercise = await GetExerciseByNameAsync(exerciseName);
+
+            var user = await userManagerService.GetUserByEmailAsync(userEmail);
+
+            if (exercise != null && user != null)
+            {
+                var userExercise = new UserExercise()
+                {
+                    UserId = user.Id,
+                    User = user,
+                    ExerciseId = exercise.Id,
+                    Exercise = exercise,
+                };
+
+                repo.Delete(userExercise);
+                await repo.SaveChangesAsync();
+            }
+        }
+    }
+}
