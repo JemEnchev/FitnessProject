5f6c1b12a9cd0f549334e5c4be702d1f0bdd370a
JemEnchev <dsenchev@abv.bg>
2022-04-28 02:05:36 +0300

    Added Image property to Exercise

---
+++ FitnessProject.Infrastructure/Migrations/20220427175035_AddImageToExercise.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessProject.Infrastructure.Migrations
+{
+    public partial class AddImageToExercise : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Exercises",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Exercises");
+        }
+    }
+}
