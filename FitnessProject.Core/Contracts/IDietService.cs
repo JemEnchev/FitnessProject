40b0cac6ce0974da117011baf480df0940c55ddb
JemEnchev <dsenchev@abv.bg>
2022-04-30 02:55:49 +0300

    Added Diet functionality

---
+++ FitnessProject.Core/Contracts/IDietService.cs
@@ -0,0 +1,24 @@
+﻿namespace FitnessProject.Core.Contracts
+{
+    using FitnessProject.Core.Models.Diet;
+    using FitnessProject.Infrastructure.Data.Models;
+
+    public interface IDietService
+    {
+        Task<IEnumerable<Diet_VM>> GetAllDietsAsync(string userEmail);
+
+        Task CreateDietAsync(Diet_VM model, string userId);
+
+        Task DeleteDietAsync(string dietName);
+
+        Task<Diet> GetDietByNameAsync(string dietName);
+
+        //Task<ICollection<SelectListItem>> PopulateDietsAsync();
+
+        //Task AddFoodToDiet(DietFood_VM model);
+
+        //Task AddSupplementToDiet(DietSupplement_VM model);
+
+        //Task<Diet> GetDietByIdAsync(string dietId);
+    }
+}
---
+++ FitnessProject.Core/Models/Diet/Diet_VM.cs
@@ -0,0 +1,26 @@
+﻿namespace FitnessProject.Core.Models.Diet
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Diet_VM
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Breakfast { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Lunch { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Dinner { get; set; }
+    }
+}
